ad1366a2448b97e04aed3c0f8d0b8a7175a742d3
Alfredo Alas <alfredo.alas14@gmail.com>
2024-07-09 09:54:16 -0600

    add Pregunta.java

---
+++ src/main/java/com/curso/microservicios/spring/examenes/models/entity/Pregunta.java
@@ -0,0 +1,39 @@
+package com.curso.microservicios.spring.examenes.models.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "preguntas")
+public class Pregunta {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "texto")
+	private String texto;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+	
+	
+	
+}
