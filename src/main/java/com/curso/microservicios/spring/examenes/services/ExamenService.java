a28393260897addf2a232286b9a01565b75cd1d8
Alfredo Alas <alfredo.alas14@gmail.com>
2024-07-09 10:44:15 -0600

    add ExamenService.java

---
+++ src/main/java/com/curso/microservicios/spring/examenes/services/ExamenService.java
@@ -0,0 +1,8 @@
+package com.curso.microservicios.spring.examenes.services;
+
+import com.curso.microservicios.spring.commons.services.CommonService;
+import com.curso.microservicios.spring.examenes.models.entity.Examen;
+
+public interface ExamenService extends CommonService<Examen> {
+
+}
