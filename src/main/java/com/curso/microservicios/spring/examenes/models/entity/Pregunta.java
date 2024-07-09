0bf189dac76ce7e1820e731a93e18b4d4ad96e4e
Alfredo Alas <alfredo.alas14@gmail.com>
2024-07-09 10:13:50 -0600

    add @ManyToOne

--- src/main/java/com/curso/microservicios/spring/examenes/models/entity/Pregunta.java
+++ src/main/java/com/curso/microservicios/spring/examenes/models/entity/Pregunta.java
@@ -2,10 +2,16 @@ package com.curso.microservicios.spring.examenes.models.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
+import com.curso.microservicios.spring.generic.alumnos.models.entity.Alumno;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "preguntas")
@@ -14,9 +20,14 @@ public class Pregunta {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@Column(name = "texto")
 	private String texto;
+
+	@JsonIgnoreProperties(value = { "preguntas" })
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "examen_id")
+	private Examen examen;
 
 	public Long getId() {
 		return id;
@@ -33,7 +44,25 @@ public class Pregunta {
 	public void setTexto(String texto) {
 		this.texto = texto;
 	}
-	
-	
-	
+
+	public Examen getExamen() {
+		return examen;
+	}
+
+	public void setExamen(Examen examen) {
+		this.examen = examen;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof Alumno)) {
+			return false;
+		}
+		Pregunta objPregunta = (Pregunta) obj;
+		return this.id != null && this.id.equals(objPregunta.getId());
+	}
+
 }
