9b63de3dd6395f53664eee1c53b396eda4701580
Alfredo Alas <alfredo.alas14@gmail.com>
2024-07-09 10:13:39 -0600

    add @OneToMany

--- src/main/java/com/curso/microservicios/spring/examenes/models/entity/Examen.java
+++ src/main/java/com/curso/microservicios/spring/examenes/models/entity/Examen.java
@@ -1,16 +1,23 @@
 package com.curso.microservicios.spring.examenes.models.entity;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "examenes")
@@ -26,6 +33,14 @@ public class Examen {
 	@Column(name = "create_at")
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date createAt;
+
+	@JsonIgnoreProperties(value = { "examen" }, allowSetters = true)
+	@OneToMany(mappedBy = "examen", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Pregunta> preguntas;
+
+	public Examen() {
+		this.preguntas = new ArrayList<>();
+	}
 
 	@PrePersist
 	public void prePersist() {
@@ -55,7 +70,24 @@ public class Examen {
 	public void setCreateAt(Date createAt) {
 		this.createAt = createAt;
 	}
-	
-	
+
+	public List<Pregunta> getPreguntas() {
+		return preguntas;
+	}
+
+	public void setPreguntas(List<Pregunta> preguntas) {
+		this.preguntas.clear();
+		preguntas.forEach(this::addPregunta);// Expresion lambda
+	}
+
+	public void addPregunta(Pregunta pregunta) {
+		this.preguntas.add(pregunta);
+		pregunta.setExamen(this);
+	}
+
+	public void removePregunta(Pregunta pregunta) {
+		this.preguntas.remove(pregunta);
+		pregunta.setExamen(null);
+	}
 
 }
