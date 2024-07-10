b43fc85db2f5ef3e8b5755dd8f4bb114e50b213f
Alfredo Alas <alfredo.alas14@gmail.com>
2024-07-09 23:03:30 -0600

    implement findByNombre method

--- src/main/java/com/curso/microservicios/spring/examenes/services/ExamenServiceImpl.java
+++ src/main/java/com/curso/microservicios/spring/examenes/services/ExamenServiceImpl.java
@@ -1,6 +1,9 @@
 package com.curso.microservicios.spring.examenes.services;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.curso.microservicios.spring.commons.services.CommonServiceImpl;
 import com.curso.microservicios.spring.examenes.models.entity.Examen;
@@ -8,5 +11,11 @@ import com.curso.microservicios.spring.examenes.models.repository.ExamenReposito
 
 @Service
 public class ExamenServiceImpl extends CommonServiceImpl<Examen, ExamenRepository> implements ExamenService {
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<Examen> findByNombre(String term) {
+		return repository.findByNombre(term);
+	}
 
 }
