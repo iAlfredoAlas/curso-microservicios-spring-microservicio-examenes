822980d45160c4b82f964db44feb30736da1899a
Alfredo Alas <alfredo.alas14@gmail.com>
2024-07-09 23:02:24 -0600

    add findByNombre method

--- src/main/java/com/curso/microservicios/spring/examenes/services/ExamenService.java
+++ src/main/java/com/curso/microservicios/spring/examenes/services/ExamenService.java
@@ -1,8 +1,12 @@
 package com.curso.microservicios.spring.examenes.services;
 
+import java.util.List;
+
 import com.curso.microservicios.spring.commons.services.CommonService;
 import com.curso.microservicios.spring.examenes.models.entity.Examen;
 
 public interface ExamenService extends CommonService<Examen> {
+	
+	public List<Examen> findByNombre(String term);
 
 }
