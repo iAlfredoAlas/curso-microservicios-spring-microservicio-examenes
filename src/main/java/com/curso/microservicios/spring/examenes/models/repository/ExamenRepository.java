d6fd753e8e54339390cb550e1330144f34fc605f
Alfredo Alas <alfredo.alas14@gmail.com>
2024-07-09 23:01:36 -0600

    add method findByNombre

--- src/main/java/com/curso/microservicios/spring/examenes/models/repository/ExamenRepository.java
+++ src/main/java/com/curso/microservicios/spring/examenes/models/repository/ExamenRepository.java
@@ -1,9 +1,15 @@
 package com.curso.microservicios.spring.examenes.models.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.curso.microservicios.spring.examenes.models.entity.Examen;
 
 public interface ExamenRepository extends CrudRepository<Examen, Long> {
 
+	@Query("select exam from Examen exam where exam.nombre like %?1%")
+	public List<Examen> findByNombre(String term);
+	
 }
