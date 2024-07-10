d4f4f4eaf460d3820f163436ba675ddca3fdad05
Alfredo Alas <alfredo.alas14@gmail.com>
2024-07-09 22:59:09 -0600

    add entity scan to generic-examenes

--- src/main/java/com/curso/microservicios/spring/examenes/CursoMicroserviciosSpringMicroservicioExamenesApplication.java
+++ src/main/java/com/curso/microservicios/spring/examenes/CursoMicroserviciosSpringMicroservicioExamenesApplication.java
@@ -2,12 +2,14 @@ package com.curso.microservicios.spring.examenes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import io.github.cdimascio.dotenv.Dotenv;
 
 @EnableEurekaClient
 @SpringBootApplication
+@EntityScan({"com.curso.microservicios.spring.generic.examenes.models.entity"})
 public class CursoMicroserviciosSpringMicroservicioExamenesApplication {
 
 	public static void main(String[] args) {
