8d7958b6a0b9b6e22075ab3140828f5162cd8ec4
Alfredo Alas <alfredo.alas14@gmail.com>
2024-07-16 11:26:12 -0600

    add method findAllAsignaturas

--- src/main/java/com/curso/microservicios/spring/examenes/services/ExamenService.java
+++ src/main/java/com/curso/microservicios/spring/examenes/services/ExamenService.java
@@ -3,10 +3,13 @@ package com.curso.microservicios.spring.examenes.services;
 import java.util.List;
 
 import com.curso.microservicios.spring.commons.services.CommonService;
+import com.curso.microservicios.spring.generic.examenes.models.entity.Asignatura;
 import com.curso.microservicios.spring.generic.examenes.models.entity.Examen;
 
 public interface ExamenService extends CommonService<Examen> {
 	
 	public List<Examen> findByNombre(String term);
+	
+	public Iterable<Asignatura> findAllAsignaturas();
 
 }
