827c4f6746f0830512dbd7d81aa8482fae8d54b8
Alfredo Alas <alfredo.alas14@gmail.com>
2024-08-07 10:08:44 -0600

    change extends to PagingAndSortingRepository

--- src/main/java/com/curso/microservicios/spring/examenes/models/repository/ExamenRepository.java
+++ src/main/java/com/curso/microservicios/spring/examenes/models/repository/ExamenRepository.java
@@ -3,11 +3,11 @@ package com.curso.microservicios.spring.examenes.models.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.curso.microservicios.spring.generic.examenes.models.entity.Examen;
 
-public interface ExamenRepository extends CrudRepository<Examen, Long> {
+public interface ExamenRepository extends PagingAndSortingRepository<Examen, Long> {
 
 	@Query("select exam from Examen exam where exam.nombre like %?1%")
 	public List<Examen> findByNombre(String term);
